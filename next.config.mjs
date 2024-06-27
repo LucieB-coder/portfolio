3db5e2b0f6c8413cf1966bf52b9e0991aa901fce
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2024-06-27 10:17:21 +0200

    🔧 Update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    trailingSlash: true, // Ensure URLs are correctly handled with trailing slashes  
+};
 
 export default nextConfig;
