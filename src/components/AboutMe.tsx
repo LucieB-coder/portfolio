8e134ecc7380663783546625bd6a7f1e5f438bfd
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2024-06-29 17:57:54 +0200

    💄 AboutMe styles section updates

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,18 +7,18 @@ const AboutMe = () => {
         <div className="flex flex-1 flex-col items-center py-12 min-h-screen bg-background-800">
             <h3 className="flex font-Literata tracking-widest text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center">ABOUT ME</h3>
             <div className="px-5 md:px-12 lg:px-26 xl:px-32">
-                <div className="flex flex-col md:flex-row rounded-3xl p-12 space-y-12 md:space-x-12 justify-center items-center md:py-32">
+                <div className="flex flex-col md:flex-row p-12 space-y-12 md:space-x-12 justify-center items-center md:py-14">
                     <p className="flex flex-auto text-md md:text-lg text-center md:text-start">{"Hello and welcome to my portfolio! I am Lucie, a full stack developer with specialized expertise in ReactJS and React Native. Currently, I am in a work-study program, balancing my time between BECOMS and the Clermont-Auvergne University Institute of Technology, where I am in my final year of a Bachelor's degree in Computer Science. I have the ability to build both websites and mobile applications. My passion for technology drives me to continually seek new knowledge and enhance my skills. My enthusiasm and curiosity inspire me to explore new horizons and tackle exciting challenges."}</p>
                     <div className="h-64 xl:h-72 aspect-square bg-primary-600 rounded-3xl" >
                         <img className="object-cover overflow-hidden h-64 xl:h-72 bg-primary-600 rounded-3xl" src={me} alt="me" />
                     </div>
                 </div>
 
-                <div className="flex flex-col rounded-3xl px-5 space-y-12 justify-center items-center">
+                <div className="flex flex-col rounded-3xl px-5 space-y-12 justify-center items-center pb-12">
                     <h5 className="flex font-Literata text-primary-500 tracking-widest text-2xl md:text-3xl lg:text-4xl text-center">What about my personnal interests ?</h5>
                     <div className="flex flex-col md:flex-row space-y-5 md:space-x-10 justify-center items-center">
-                        <p className="flex flex-1 text-md md:text-lg text-center md:text-start">{"Outside of my interests in development and current technologies, I have many hobbies. I love to read, play video games (I am a huge fan of the Diablo series), and I am very curious about anything related to astrophysics. I also love plants, and much more. For the past year, I’ve been teaching myself to play chess. There is still a long road ahead, but I am slowly becoming a challenging opponent. If you dare, click on the button and confront me for a game on Chess.com!"}</p>
-                        <a href="https://www.chess.com/member/lulu-la-tortue" className="flex flex-1 flex-no-wrap items-center space-x-3">
+                        <p className="flex flex-auto text-md  bg-ink-200 md:text-lg text-center md:text-start">{"Outside of my interests in development and current technologies, I have many hobbies. I love to read, play video games (I am a huge fan of the Diablo series), and I am very curious about anything related to astrophysics. I also love plants, and much more. For the past year, I’ve been teaching myself to play chess. There is still a long road ahead, but I am slowly becoming a challenging opponent. If you dare, click on the button and confront me for a game on Chess.com!"}</p>
+                        <a href="https://www.chess.com/member/lulu-la-tortue" className="flex flex-none flex-no-wrap items-center space-x-3">
                             <img className="h-10 object-contain" src={chessDotComLogo} alt="chessdotcomlogo" />
                             <button className="p-[3px] relative flex flex-wrap">
                                 <div className="absolute inset-0 bg-gradient-to-r from-primary-500 to-primary-300 rounded-lg" />
