4d227c1e549ddf59c733fdb814a1b1c0f9b558d9
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2024-06-29 13:08:57 +0200

    💄 Update Welcome page styles for better responsive design

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -26,7 +26,7 @@ const Welcome = () => {
 
     return (
         <>
-            {windowSize == "sm" ? (
+            {windowSize === "sm" ? (
                 <div className="flex flex-col items-center pt-8 px-8">
                     <div className="flex flex-1 w-full items-start"><p className="lg:text-xl font-bold opacity-80">CONTACT</p></div>
                     <div className="flex flex-col items-center space-y-4 pt-10 md:p-24 lg:p-10 xl:pb-8 xl:pt-0 xl:px-0">
@@ -58,14 +58,14 @@ const Welcome = () => {
                     </div>
                 </div>
             ) : (
-                <div className="flex flex-col max-h-screen items-center pt-8 px-8">
-                    <div className="flex flex-1 w-full items-start"><p className="lg:text-xl font-bold opacity-80">CONTACT</p></div>
-                    <div className="flex flex-col items-center space-y-4">
-                        <h1 className="flex font-Literata tracking-widest text-5xl md:text-7xl  lg:text-8xl xl:text-[8rem] text-center">LUCIE BEDOURET</h1>
+                <div className="flex flex-col min-h-screen max-h-screen items-center justify-between py-8 px-8">
+                    <div className="flex w-full items-start"><p className="lg:text-xl font-bold opacity-80">CONTACT</p></div>
+                    <div className="flex flex-1 flex-col items-center justify-center space-y-2">
+                        <h1 className="flex font-Literata tracking-widest text-7xl lg:text-8xl xl:text-[8rem] text-center">LUCIE BEDOURET</h1>
                         <p className="tracking-widest opacity-80">~ FULL STACK DEVELOPER ~</p>
                     </div>
                     <div className="flex flex-auto">
-                        <div className="grid grid-cols-3 gap-5 relative py-8">
+                        <div className="grid grid-cols-3 gap-5 relative">
                             <div className="relative flex flex-1 h-full rounded-3xl overflow-hidden" >
                                 <img className="object-cover overflow-hidden" src={me} alt="me" />
                                 <div className="absolute w-full h-full bg-black opacity-25" />
@@ -78,12 +78,12 @@ const Welcome = () => {
                                     <p className="absolute top-5 left-5 font-bold lg:text-2xl">PROJECTS</p>
                                 </div>
                                 <div className="relative flex flex-none rounded-3xl overflow-hidden" >
-                                    <img className="absolute w-full object-cover object-center overflow-hidden" src={skillsImage} alt="skills" />
+                                    <img className="absolute w-full h-full object-cover object-center overflow-hidden" src={skillsImage} alt="skills" />
                                     <div className="absolute w-full h-full bg-black opacity-25" />
                                     <p className="absolute top-5 left-5 text-background-500 font-bold lg:text-2xl">SKILLS</p>
                                 </div>
                                 <div className="relative flex flex-none rounded-3xl overflow-hidden" >
-                                    <img className="absolute w-full object-cover object-center overflow-hidden" src={resumeImage} alt="skills" />
+                                    <img className="absolute w-full h-full object-cover object-center overflow-hidden" src={resumeImage} alt="skills" />
                                     <div className="absolute w-full h-full bg-black opacity-25" />
                                     <p className="absolute top-5 left-5 font-bold lg:text-2xl">RESUME</p>
                                 </div>
